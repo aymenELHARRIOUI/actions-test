f186efbc8ed7710f3d88f20c8512a606ba957431
wiaym <wiaym1004@gmail.com>
2023-12-12 00:22:38 +0100

    2er commit

--- main.py
+++ main.py
@@ -18,14 +18,14 @@ logger_file_handler.setFormatter(formatter)
 logger.addHandler(logger_file_handler)
 
 try:
-    SOME_SECRET = os.environ["SOME_SECRET"]
+    sec = os.environ["sec"]
 except KeyError:
-    SOME_SECRET = "Token not available!"
-    # logger.info("Token not available!")
+    sec = "Token not available!"
+    # logger.info("Token Manche!!!!!")
     # raise
 
 if __name__ == "__main__":
-    logger.info(f"Token value: {SOME_SECRET}")
+    logger.info(f"Token value: {sec}")
 
     r = requests.get('https://weather.talkpython.fm/api/weather/?city=Rabat&country=MA')
     if r.status_code == 200:
@@ -33,32 +33,3 @@ if __name__ == "__main__":
         temperature = data["forecast"]["temp"]
         logger.info(f'Weather in Rabat: {temperature}')
 
-
-"""
-
-import logging.handlers
-import requests
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
-)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
-
-
-
-if __name__ == "__main__":
-
-    r = requests.get('https://weather.talkpython.fm/api/weather/?city=Rabat&country=MA')
-
-    if r.status_code == 200:
-        data = r.json()
-        temperature = data["forecast"]["temp"]
-        logger.info(f'Weather in Rabat: {temperature}')
-"""
